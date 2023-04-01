e01396e9911e3425bda663bc0d8d30eab959d734
AlexJPrill <alex.j.prill@gmail.com>
2023-04-01 03:12:02 -0500

    push

--- RestAPI.py
+++ RestAPI.py
@@ -3,12 +3,32 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-totalCarbon = 20
-glassBottles = 50
-cans = 40
-totalItems = 77
-plasticBottles = 45
+totalCarbon = 0
+glassBottles = 0
+cans = 0
+totalItems = 0
+plasticBottles = 0
 
+@app.route("/glass")
+def updateglass():
+    glassBottles = glassBottles + 1
+    print(glassBottles)
+
+@app.route("/carbon", methods=["POST"])
+def updateCarbon():
+    totalCarbon = totalCarbon + 1
+
+@app.route("/cans", methods=["POST"])
+def updateCans():
+    cans = cans + 1
+
+@app.route("/total", methods=["POST"])
+def updateItems():
+    totalItems = totalItems + 1
+
+@app.route("/plastic", methods=["POST"])
+def updatePlastic():
+    plasticBottles = plasticBottles + 1
 
 
 @app.route("/data")
