a2466823c5885d3a36429cda45ef2103a7debdd8
SamForrestB <samforrestb@gmail.com>
2023-04-01 04:50:20 -0500

    Monitor display full screen

--- display/MonitorDisplay.py
+++ display/MonitorDisplay.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def displayGlass():
     glassbin = cv2.imread("Images\glassbin.jpg", cv2.IMREAD_ANYCOLOR)
     cv2.imshow("Please place your waste in the Yellow, 'Glass' Bin", glassbin)
-    cv2.setWindowProperty("Please place your waste in the Yellow, 'Glass' Bin", cv2.WND_PROP_TOPMOST, 1)
+    cv2.setWindowProperty("Please place your waste in the Yellow, 'Glass' Bin", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.waitKey(1)
     time.sleep(5)
     cv2.destroyAllWindows()
@@ -20,7 +20,7 @@ def displayGlass():
 def displayPlastic():
     plasticbin = cv2.imread("Images\plasticbin.jpg", cv2.IMREAD_ANYCOLOR)
     cv2.imshow("Please place your waste in the Blue, 'Plastic' Bin", plasticbin)
-    cv2.setWindowProperty("Please place your waste in the Blue, 'Plastic' Bin", cv2.WND_PROP_TOPMOST, 1)
+    cv2.setWindowProperty("Please place your waste in the Blue, 'Plastic' Bin", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.waitKey(1)
     time.sleep(5)
     cv2.destroyAllWindows()
@@ -30,7 +30,7 @@ def displayPlastic():
 def displayMetal():
     metalbin = cv2.imread("Images\metalbin.jpg", cv2.IMREAD_ANYCOLOR)
     cv2.imshow("Please place your waste in the Red, 'Metal' Bin", metalbin)
-    cv2.setWindowProperty("Please place your waste in the Red, 'Metal' Bin", cv2.WND_PROP_TOPMOST, 1)
+    cv2.setWindowProperty("Please place your waste in the Red, 'Metal' Bin", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.waitKey(1)
     time.sleep(5)
     cv2.destroyAllWindows()
